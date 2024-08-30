b298ce9de94c175b9f5b5e80fb3062bfa893921c
barthalomena <barthalomena17@gmail.com>
2024-08-30 20:46:41 +0530

    heroImage

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,21 @@
 import HeroImg from '../assets/Blowing a dandelion-amico.png'
+import { AiOutlineTwitter,AiOutlineFacebook,AiOutlineLinkedin,AiOutlineGithub } from "react-icons/ai";
 export default function Hero(){
+    
     return <section className='flex px-5 py-32 bg-secondary justify-center'>
-        <h1 className='w-1/2 text-white text-4xl '>Hi, <br/> I am Barthalomena 
+       <div className='w-1/2 flex flex-col'>
+        <h1 className='text-white text-6xl font-hero-font'>Hi, <br/> I am <span className='text-black'> Barthalomena </span> Francis
         <p className='text-2xl'>I am a Tester</p>
          </h1>
+        <div className='flex py-10'>
+            <a href='#' className='pr-5 hover text-white'><AiOutlineTwitter size={40}/></a>
+            <a href='#' className='pr-5 hover text-white' ><AiOutlineFacebook size={40}/></a>
+            <a href='#' className='pr-5 hover text-white'><AiOutlineLinkedin size={40}/></a>
+            <a href='#' className='hover text-white' ><AiOutlineGithub size={40}/></a>
+
+
+        </div>
+         </div>
          <img className='w-1/3' src={HeroImg}/>
     </section>
 }
