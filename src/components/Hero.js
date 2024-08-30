74b4f3a733d84502f3a5daa07cfc96d3b267626c
barthalomena <barthalomena17@gmail.com>
2024-08-30 21:46:46 +0530

    hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,20 +2,28 @@ import HeroImg from '../assets/Blowing a dandelion-amico.png'
 import { AiOutlineTwitter,AiOutlineFacebook,AiOutlineLinkedin,AiOutlineGithub } from "react-icons/ai";
 export default function Hero(){
     
-    return <section className='flex px-5 py-32 bg-secondary justify-center'>
-       <div className='w-1/2 flex flex-col'>
-        <h1 className='text-white text-6xl font-hero-font'>Hi, <br/> I am <span className='text-black'> Barthalomena </span> Francis
-        <p className='text-2xl'>I am a Tester</p>
-         </h1>
-        <div className='flex py-10'>
-            <a href='#' className='pr-5 hover text-white'><AiOutlineTwitter size={40}/></a>
-            <a href='#' className='pr-5 hover text-white' ><AiOutlineFacebook size={40}/></a>
-            <a href='#' className='pr-5 hover text-white'><AiOutlineLinkedin size={40}/></a>
-            <a href='#' className='hover text-white' ><AiOutlineGithub size={40}/></a>
+   return <section className='flex flex-col md:flex-row px-5 py-32 bg-secondary justify-center'>
+       <div className='md:w-1/2 flex flex-col'>
+    <h1 className='text-white text-6xl font-hero-font'>
+        Hi, <br/> I am <span className='text-black'> Barthalomena </span> <p className='text-2xl'>I am a Tester</p>
+    </h1>
+    
+    <div className='flex py-10'>
+        <a href='#' className='pr-5 hover:text-white'>
+            <AiOutlineTwitter size={40}/>
+        </a>
+        <a href='#' className='pr-5 hover:text-white'>
+            <AiOutlineFacebook size={40}/>
+        </a>
+        <a href='#' className='pr-5 hover:text-white'>
+            <AiOutlineLinkedin size={40}/>
+        </a>
+        <a href='#' className='hover:text-white'>
+            <AiOutlineGithub size={40}/>
+        </a>
+    </div>
+</div>
+<img className='md:w-1/3' src={HeroImg} alt='Hero' />
 
-
-        </div>
-         </div>
-         <img className='w-1/3' src={HeroImg}/>
     </section>
 }
