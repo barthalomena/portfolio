c451d869eea280f88b0c18bf65dfa790f37ed7dc
barthalomena <barthalomena17@gmail.com>
2024-08-31 17:05:52 +0530

    resume updated

--- src/components/resume.js
+++ src/components/resume.js
@@ -8,7 +8,7 @@ return<section className='flex flex-col md:flex-row bg-secondary px-5'>
     <div className='md:w-1/2 flex-col flex justify-center'>
     <div className='flex flex-col justify-center text-white'>
         <h1 className='text-4xl  border-b-4  border-borderColor mb-5 w-[140px] font-bold'>Resume</h1>
-        <p className='pb-5'>You can review my resume  <a className='btn hover' href='#'> Download </a> </p>
+        <p className='pb-5'>You can review my resume  <a className='btn' href='#'> Download </a> </p>
          </div>
     </div>
 </section>
